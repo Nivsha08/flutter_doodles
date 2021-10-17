671e3140f9d89f116e18cac90ce21304a630591c
Niv Shani <nivshani08@gmail.com>
2021-10-17 21:18:01 +0300

    alignment

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -35,31 +35,33 @@ class QuizApplication extends StatelessWidget {
           backgroundColor: Colors.teal[700],
           title: Text('Quiz Application'),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('A question goes here'),
-            AnswerButton(
-              answerNum: 1,
-              child: Text('Custom answer'),
-              onPressed: () => onAnswerClick(1),
-            ),
-            AnswerButton(
-              answerNum: 2,
-              child: Text('Custom answer'),
-              onPressed: () => onAnswerClick(2),
-            ),
-            AnswerButton(
-              answerNum: 3,
-              child: Text('Custom answer'),
-              onPressed: () => onAnswerClick(3),
-            ),
-            AnswerButton(
-              answerNum: 4,
-              child: Text('Custom answer'),
-              onPressed: () => onAnswerClick(4),
-            ),
-          ],
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('A question goes here'),
+              AnswerButton(
+                answerNum: 1,
+                child: Text('Custom answer'),
+                onPressed: () => onAnswerClick(1),
+              ),
+              AnswerButton(
+                answerNum: 2,
+                child: Text('Custom answer'),
+                onPressed: () => onAnswerClick(2),
+              ),
+              AnswerButton(
+                answerNum: 3,
+                child: Text('Custom answer'),
+                onPressed: () => onAnswerClick(3),
+              ),
+              AnswerButton(
+                answerNum: 4,
+                child: Text('Custom answer'),
+                onPressed: () => onAnswerClick(4),
+              ),
+            ],
+          ),
         ),
       ),
     );
